a897fec5493a6180c55aee3d5d3036138f4f8ee7
r.bonk907@gmail.com <r.bonk907@gmail.com>
2023-01-19 17:21:10 -0900

    create experience section

---
+++ components/experience.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Experience.module.css';
+
+const Experience = () => {
+    return (
+        <div className={styles.experience}>
+            <h2>Experience</h2>
+        </div>
+    );
+};
+
+export default Experience;
