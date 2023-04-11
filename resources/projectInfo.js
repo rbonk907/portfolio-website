64b5d29c107d1ee192285017a03fe9a708e4c9e1
r.bonk907@gmail.com <r.bonk907@gmail.com>
2023-04-11 11:37:20 -0800

    Update project cards

--- resources/projectInfo.js
+++ resources/projectInfo.js
@@ -2,35 +2,20 @@ export const projectInfo = {
     projects: [
         {
             id: 1,
-            title: "Project 1",
-            description: "Eu nisi anim exercitation minim proident aliqua. Est excepteur occaecat enim in ullamco Lorem commodo exercitation dolor.",
+            title: "Reddit + Redux",
+            description: "Minimalistic frontend application to display information from the Reddit JSON API",
             imageURL: "/missing-image.webp",
-            gitHubLink: '',
-            tags: [ 'C', 'ARM', 'RTOS' ],
+            gitHubLink: 'https://github.com/rbonk907/reddit-api-project',
+            tags: [ 'React', 'Redux', 'TypeScript' ],
         },
         {
             id: 2,
-            title: "Project 2",
-            description: "Eu nisi anim exercitation minim proident aliqua. Est excepteur occaecat enim in ullamco Lorem commodo exercitation dolor.",
+            title: "E-Commerce REST API",
+            description: "RESTful API for a fake e-commerce site selling software related decals. Connects to a PostgreSQL database and allows users to sign-up, and login with Oauth2.0. Users can select from a range of stickers and add them to a shopping cart",
             imageURL: "/missing-image.webp",
             gitHubLink: '',
-            tags: [ 'C', 'Embedded' ]
+            tags: [ 'NodeJS', 'ExpressJS', 'PostgreSQL' ]
         },
-        {
-            id: 3,
-            title: "Project 3",
-            description: "Eu nisi anim exercitation minim proident aliqua. Est excepteur occaecat enim in ullamco Lorem commodo exercitation dolor.",
-            imageURL: "/missing-image.webp",
-            gitHubLink: '',
-            tags: [ 'JavaScript', 'React', 'Frontend', 'Web App' ],
-        },
-        {
-            id: 4,
-            title: "Project 4",
-            description: "Eu nisi anim exercitation minim proident aliqua. Est excepteur occaecat enim in ullamco Lorem commodo exercitation dolor.",
-            imageURL: "/missing-image.webp",
-            gitHubLink: '',
-            tags: [ 'JavaScript', 'React', 'Next.js', 'Frontend', 'Web App' ],
-        }
+        
     ]
 }
