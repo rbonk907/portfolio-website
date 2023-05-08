277b47883d47ca916f4c294208b402ec0f8375ad
rbonk907 <40987927+rbonk907@users.noreply.github.com>
2023-05-08 12:28:16 -0800

    create page for reddit redux project

--- resources/projectInfo.js
+++ resources/projectInfo.js
@@ -3,6 +3,7 @@ export const projectInfo = {
         {
             id: 1,
             title: "Reddit + Redux",
+            href: "/projects/redditRedux",
             description: "Minimalistic frontend application to display information from the Reddit JSON API",
             imageURL: "/missing-image.webp",
             gitHubLink: 'https://github.com/rbonk907/reddit-api-project',
@@ -12,6 +13,7 @@ export const projectInfo = {
         {
             id: 2,
             title: "E-Commerce REST API",
+            href: "",
             description: "RESTful API for a fake e-commerce site selling software related decals. Connects to a PostgreSQL database and allows users to sign-up, and login with Oauth2.0. Users can select from a range of stickers and add them to a shopping cart",
             imageURL: "/missing-image.webp",
             gitHubLink: '',
@@ -21,6 +23,7 @@ export const projectInfo = {
         {
             id: 3,
             title: "Game Boy Emulator",
+            href: "",
             description: "Game Boy emulations have been done numerous times, but this one is mine. Written in C and uses SDL for window management",
             imageURL: "/missing-image.webp",
             gitHubLink: '',
